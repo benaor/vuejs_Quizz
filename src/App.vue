b79b953756a99865087dba3b23723ef64e58bb6a
benaor <benjamingirard25@gmail.com>
2022-03-16 11:58:36 +0100

    cleanning vue App

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,28 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div>
+    <h1>microphone can be used not only to pick up sound, but also to project sound similar to a speaker.</h1>
+
+    <input type="radio" name="options" value="true" />
+    <label>True</label>
+    <br />
+
+    <input type="radio" name="options" value="false" />
+    <label>False</label>
+    <br />
+
+    <button class="send" type="button">Send</button>
+    <br />
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
 
 export default defineComponent({
   name: 'App',
-  components: {
-    HelloWorld
-  }
 });
+
+// https://opentdb.com/api.php?amount=1
 </script>
 
 <style lang="scss">
@@ -22,6 +32,22 @@ export default defineComponent({
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 60px auto;
+  max-width: 960px;
+
+  input[type="radio"] {
+    margin: 12px 4px;
+  }
+
+  button.send {
+    margin-top: 12px;
+    height: 40px;
+    min-width: 120px;
+    padding: 0 16px;
+    color: #fff;
+    background-color: #1867c0;
+    border: 1px solid #1867c0;
+    cursor: pointer;
+  }
 }
 </style>
